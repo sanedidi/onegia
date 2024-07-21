390a971e02f0c4b182c3a04e2bf350737158b934
Shaxzod <sanedidi98@gmail.com>
2024-07-21 13:27:57 +0500

    add: hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import s from './styles.module.scss'
+import React from "react";
+import s from "./styles.module.scss";
+import absImg from '../../assets/img/hero-abs.png'
 const Hero = () => {
     return (
         <div className={s.hero}>
             <div className="container">
                 <div className={s.hero__wrapper}>
-
+                    <div className={s.hero__content}>
+                        <h2 className={s.hero__title}>
+                            Unlock your engineering potential.
+                        </h2>
+                        <p className={s.hero__text}>
+                            Join our expert network and work with leading manufacturing and
+                            industrial companies on diverse engineering projects, in short
+                            side-gigs or longer term engagements.
+                        </p>
+                        <button>
+                            Join Now
+                        </button>
+                    <img className={s.hero__img} src={absImg} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
