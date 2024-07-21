b7362611e723359b897910505a09ca9d6e8c1e31
Shaxzod <sanedidi98@gmail.com>
2024-07-21 18:39:37 +0500

    update 3 files and create 1 file

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import s from "./styles.module.scss";
 import logo from "../../assets/svg/logo.png";
+import line from "../../assets/img/line.png";
 import { workCard } from './components/index'
+import { BsArrowRight } from "react-icons/bs";
 const Work = () => {
     return (
         <div className={s.work}>
@@ -9,19 +11,34 @@ const Work = () => {
                 <div className={s.work__wrapper}>
                     <div className={s.work__title}>
                         <h2>How</h2>
-                        {/* <img src={logo} alt="" /> */}
+                        <img src={logo} alt="" />
                         <h2>works</h2>
                     </div>
-                    <div className={s.word__cards}>
+                    <div className={s.work__cards}>
                         {workCard.map((el) => {
                             return (
                                 <div key={el.id} className={s.work__card}>
-                                    <div className={s.work__card_title}>
-                                        <h2>{el.id}</h2>
+                                    <div className={s.work__card_num}>
+                                        <h2>{el.id}.</h2>
+                                        <div className={el.id === 4 ? s.noLine : s.work__container}>
+                                            <div className={s.work__line}></div>
+                                            <div className={s.work__arrow_right}></div>
+                                        </div>
+
+                                    </div>
+                                    <div className={s.work__text}>
+                                        <div className={s.work__card_title}>
+                                            <h2>  {el.title}</h2>
+                                        </div>
+                                        <div className={s.work__card_text}>
+                                            <p>{el.text}</p>
+                                            <p>{el.text2}</p>
+                                        </div>
                                     </div>
                                 </div>
                             )
                         })}
+                        <button className={s.work__button}>Start now</button>
                     </div>
                 </div>
             </div>
