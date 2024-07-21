2457779d539f6159c2b434fbf844f15e115a50ea
Shaxzod <sanedidi98@gmail.com>
2024-07-21 13:04:05 +0500

    header responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,9 @@ import s from './styles.module.scss';
 import { links } from './components/links';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/svg/logo.png'
+import clsx from 'clsx';
+import { CiBurger } from 'react-icons/ci';
+import { BiMenu } from 'react-icons/bi';
 const Header = () => {
     const firstTwoLinks = links.slice(0, 2);
     const lastTwoLinks = links.slice(2, 4);
@@ -11,7 +14,7 @@ const Header = () => {
         <div className={s.header}>
             <div className="container">
                 <div className={s.header__wrapper}>
-                    <div className={s.header__links}>
+                    <div className={clsx(s.header__links, s.left)}>
                         {firstTwoLinks.map((el) => {
                             return (
                                 <Link className={s.header__link} key={el.id} to={el.path}>
@@ -20,19 +23,23 @@ const Header = () => {
                             );
                         })}
                     </div>
+                    <div className={s.burger}>
+                        <BiMenu fontSize={"25px"} />
+                    </div>
                     <div className={s.header__logo}>
                         <img src={logo} alt="" />
                     </div>
-                    <div className={s.header__links}>
+                    <div className={clsx(s.header__links, s.right)}>
                         {lastTwoLinks.map((el) => {
                             return (
-                                <Link className={s.header__link} key={el.id} to={el.path}>
+                                <Link className={el.name === "Join Now" ? `${clsx(s.header__link, s.join)}` : s.header__link} key={el.id} to={el.path}>
                                     {el.name}
                                 </Link>
                             );
                         })}
                     </div>
                 </div>
+
             </div>
         </div>
     );
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from './styles.module.scss'
+const Hero = () => {
+    return (
+        <div className={s.hero}>
+            <div className="container">
+                <div className={s.hero__wrapper}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
