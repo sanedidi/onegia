845f7202f3d633c96b71f5173a98184b1bed14c3
Shaxzod <147810734+sanedidi@users.noreply.github.com>
2024-07-23 20:41:41 +0500

    Update links.js

--- src/components/Header/components/links.js
+++ src/components/Header/components/links.js
@@ -1,6 +1,6 @@
 export const links = [
     { id: 1, path: "/", name: "For Engineers" },
     { id: 2, path: "/", name: "For Leaders" },
-    { id: 3, path: "/", name: "Log in" },
-    { id: 4, path: "/", name: "Join Now" },
+    // { id: 3, path: "/", name: "Log in" },
+    { id: 4, path: "/https://ongenia-react-frontend.vercel.app/", name: "Join Now" },
 ]
